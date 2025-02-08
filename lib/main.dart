3a908c2f886326d4e9b1b6da72558c589b925a94
Barzu George <George.Barzu@noventi.de>
2025-02-08 16:21:09 +0200

    Initial commit with mods

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,45 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.indigoAccent,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.deepOrangeAccent),
+                height: 100.0,
+                width: 100.0,
+                margin: EdgeInsets.all(20.0),
+                child: Text('Number of donuts eaten'),
+              ),
+              Container(
+                width: 30.0,
+                height: 40.0,
+                color: Colors.blue,
+                child: Text("Container 2"),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.red,
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
